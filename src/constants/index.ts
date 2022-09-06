2fdc292edced8227da5af470fdcda2b2e1d372a1
leephan2k1 <pttl2k1@gmail.com>
2022-09-06 18:37:05 +0700

    style: 💄 redesign header

---
+++ src/constants/index.ts
@@ -0,0 +1,8 @@
+import { Navigate } from 'types';
+
+export const quickList: Navigate[] = [
+    { title: 'Xu Hướng', link: '/' },
+    { title: 'Danh Mục', link: '/' },
+    { title: 'Danh Sách Sản Phẩm', link: '/' },
+    { title: 'Voucher', link: '/' },
+];
