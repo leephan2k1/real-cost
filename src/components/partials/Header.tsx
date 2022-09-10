ff8eba7d514ed12d62f258d34bc193d718e16e4f
leephan2k1 <pttl2k1@gmail.com>
2022-09-11 01:48:46 +0700

    style: 💄 redesign

--- src/components/partials/Header.tsx
+++ src/components/partials/Header.tsx
@@ -1,4 +1,4 @@
-import { useSetAtom } from 'jotai';
+import { useSetAtom, useAtom } from 'jotai';
 import Link from 'next/link';
 import { BiUser } from 'react-icons/bi';
 import { HiMenuAlt2 } from 'react-icons/hi';
@@ -6,23 +6,28 @@ import mobileSearchState from '~/atoms/mobileSearchState';
 import sideBarState from '~/atoms/sideBarState';
 import SearchInput from '~/components/shared/SearchInput';
 import { quickList } from '~/constants';
-
+import SearchResults from '../shared/SearchResults';
 import { BellIcon, MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import searchMarket from '~/atoms/marketSearch';
+import { Market } from 'types';
 
 import ListBoxButton from '../buttons/ListBoxButton';
 import Logo from '../icons/Logo';
 
 const Header = () => {
+    const [market, setMarket] = useAtom(searchMarket);
     const setSideBarState = useSetAtom(sideBarState);
     const setSearchModalState = useSetAtom(mobileSearchState);
 
     const handleSelect = (value: string) => {
         // not completed
-        console.error(value);
+        const market = value.toLowerCase();
+
+        setMarket(market === 'tất cả' ? ('all' as Market) : (market as Market));
     };
 
     return (
-        <header className="absolute-center fixed top-0 left-0 z-10 h-[75px] w-full bg-gradient-to-r from-indigo-500 to-violet-500 text-white md:h-[90px]">
+        <header className="absolute-center fixed top-0 left-0 z-10 h-[70px] w-full bg-gradient-to-r from-black/70 to-[#313132] text-white md:h-[85px]">
             <div className="w-max-[1300px] flex h-full w-[90%] items-center justify-between">
                 {/* mobile menu  */}
                 <button
@@ -42,13 +47,13 @@ const Header = () => {
                 </Link>
 
                 {/* search  */}
-                <div className="z-50 mx-4 flex h-fit w-1/2 items-center justify-end space-x-4 text-gray-800">
+                <div className="relative z-50 mx-4 flex h-fit w-1/2 items-center justify-end space-x-4 text-gray-800">
                     {/* search options (medium screen)  */}
                     <div className="relative hidden h-full flex-1 items-center justify-end md:flex md:justify-center lg:justify-end">
                         <ListBoxButton
                             options={['Tất cả', 'Lazada', 'Tiki', 'Shopee']}
-                            defaultOption="Lazada"
-                            style="bg-white w-32 p-2 rounded-lg focus:ring-4 focus:ring-blue-500"
+                            defaultOption={market}
+                            style="bg-white w-full p-2 rounded-lg focus:ring-2 focus:ring-grary-700"
                             handleSelect={handleSelect}
                         />
                     </div>
@@ -61,19 +66,21 @@ const Header = () => {
                         <MagnifyingGlassIcon className="h-8 w-8" />
                     </button>
 
-                    <SearchInput styles="hidden h-full w-3/4 items-center space-x-2 overflow-hidden rounded-xl bg-white p-2 shadow-xl md:flex" />
+                    <SearchInput styles="hidden h-full w-4/5 items-center space-x-2 overflow-hidden rounded-xl bg-white p-2 shadow-xl md:flex" />
+
+                    <SearchResults />
                 </div>
 
                 {/* menu desktop & user  */}
-                <div className="flex h-full w-[25%] items-center justify-evenly lg:w-1/2">
+                <div className="flex h-full w-[25%] items-center justify-evenly lg:w-[40%]">
                     {/* menu  */}
-                    <ul className="hidden font-secondary lg:flex lg:space-x-4 lg:text-2xl xl:space-x-6 xl:text-3xl">
+                    <ul className="hidden font-secondary font-light lg:flex lg:space-x-4 lg:text-xl  xl:space-x-8 xl:text-2xl">
                         {quickList.length &&
                             quickList.map((nvg) => {
                                 return (
                                     <li key={nvg.title}>
                                         <Link href={nvg.link}>
-                                            <a>Xu hướng</a>
+                                            <a>{nvg.title}</a>
                                         </Link>
                                     </li>
                                 );
