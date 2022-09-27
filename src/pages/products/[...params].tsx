58d8e46e462402ef699535c803350a50fe2a740d
leephan2k1 <pttl2k1@gmail.com>
2022-09-28 00:34:24 +0700

    feat: 🎸 fetch product details & price history

--- src/pages/products/[...params].tsx
+++ src/pages/products/[...params].tsx
@@ -1,20 +1,56 @@
 import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
-import Section from '~/components/shared/Section';
-import ProductHead from '~/components/shared/ProductHead';
+import { Else, If, Then } from 'react-if';
+import { ItemHistory, Product } from 'types';
+import ProductChart from '~/components/shared/ProductChart';
 import ProductDescription from '~/components/shared/ProductDescription';
+import ProductHead from '~/components/shared/ProductHead';
+import Section from '~/components/shared/Section';
+import {
+    getHistoryPrice,
+    getProductDetails as bcGetProductDetails,
+} from '~/serverless/models/Bc.model';
+import { getProductDetails as tkGetProductDetails } from '~/serverless/models/Tiki.model';
 
-const DetailsPage: NextPage = () => {
+interface DetailsPageProps {
+    product: Product;
+    productPriceHistory: ItemHistory | null;
+}
+
+const DetailsPage: NextPage<DetailsPageProps> = ({
+    product,
+    productPriceHistory,
+}) => {
     return (
         <div className="min-h-screen w-full pt-[100px]">
             <Section style="w-max-[1300px] h-fit mx-auto w-[90%]">
-                <ProductHead />
+                <ProductHead product={product} />
+            </Section>
+
+            <Section
+                title="Lịch sử giá"
+                style="my-10 w-max-[1300px] h-fit mx-auto w-[90%]"
+            >
+                <If condition={!!productPriceHistory}>
+                    <Then>
+                        <ProductChart
+                            productName={product?.name}
+                            itemHistory={productPriceHistory as ItemHistory}
+                        />
+                    </Then>
+
+                    <Else>
+                        <h3 className="mx-auto">
+                            Sản phẩm hiện tại chưa có lịch sử giá
+                        </h3>
+                    </Else>
+                </If>
             </Section>
 
             <Section
                 title="Mô tả"
                 style="my-10 w-max-[1300px] h-fit mx-auto w-[90%]"
             >
-                <ProductDescription />
+                <ProductDescription description={product?.description} />
             </Section>
         </div>
     );
@@ -22,12 +58,38 @@ const DetailsPage: NextPage = () => {
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-export const getStaticProps: GetStaticProps = async () => {
-    return {
-        props: {
-            data: [],
-        },
-    };
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const [market, url] = params?.params;
+
+    let product: Product | null;
+
+    switch (market) {
+        case 'tiki':
+            product = await tkGetProductDetails(url);
+            break;
+        case 'shopee':
+        case 'lazada':
+            product = await bcGetProductDetails(market, url);
+            break;
+        default:
+            return { notFound: true };
+    }
+
+    const priceHistory = await getHistoryPrice(product?.product_base_id);
+
+    if (product) {
+        return {
+            props: {
+                product,
+                productPriceHistory: priceHistory,
+            },
+            revalidate: 60 * 60 * 12, //12h
+        };
+    }
+
+    return { notFound: true };
 };
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
