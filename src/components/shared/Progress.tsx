52c42d738e48bc3241919c44829e0468d3cf2f4b
leephan2k1 <pttl2k1@gmail.com>
2022-10-03 10:29:12 +0700

    style: 💄 fix overflow text > 40%

--- src/components/shared/Progress.tsx
+++ src/components/shared/Progress.tsx
@@ -8,15 +8,15 @@ function Progress({ done }: ProgressProps) {
     return (
         <div className="my-2 flex h-[30px] w-full items-center overflow-hidden rounded-2xl bg-slate-300 lg:w-[90%]">
             <div
-                className="absolute-center h-full rounded-2xl bg-gradient-to-r from-rose-500  to-red-300 text-white"
+                className="absolute-center h-full rounded-2xl bg-gradient-to-r from-rose-500  to-red-300 text-xl text-white"
                 style={{
                     width: `${done}%`,
                 }}
             >
-                {done > 30 && `Đã bán ${done}%`}
+                {done > 35 && `Đã bán ${done}%`}
             </div>
 
-            {done <= 30 && <h4 className="px-4">Đã bán {done}%</h4>}
+            {done <= 35 && <h4 className="px-4">Đã bán {done}%</h4>}
         </div>
     );
 }
