83ae72011162cfd2444e7f054f40abda64a5f378
leephan2k1 <pttl2k1@gmail.com>
2022-10-06 14:19:49 +0700

    feat: 🎸 handle remove favorites product

--- src/components/shared/ProductCard.tsx
+++ src/components/shared/ProductCard.tsx
@@ -1,149 +1,155 @@
 import Link from 'next/link';
 import { isNumber } from 'radash';
-import { memo, useState } from 'react';
+import { memo } from 'react';
 import { Else, If, Then } from 'react-if';
 import { SearchResult } from 'types';
 import Progress from '~/components/shared/Progress';
 import { mapping_market_colors, PRODUCTS_PATH } from '~/constants';
+import useProduct from '~/context/ProductContext';
 import { handleSubPathMarket } from '~/utils/stringHandler';
 
-import { BellIcon } from '@heroicons/react/24/outline';
+import { XMarkIcon } from '@heroicons/react/24/outline';
 
 interface ProductCardProps {
     product: SearchResult;
+    isFavoriteProduct?: boolean;
 }
 
-const ProductCard = ({ product }: ProductCardProps) => {
-    const [isNotification, setIsNotification] = useState(false);
+const ProductCard = ({ product, isFavoriteProduct }: ProductCardProps) => {
+    const productCtx = useProduct();
 
-    const activeNotification = () => {
-        setIsNotification((prevState) => !prevState);
+    const handleRemoveProduct = () => {
+        if (isFavoriteProduct) {
+            productCtx?.removeProduct('single', product?.link);
+        }
     };
 
     return (
-        <Link
-            href={`/${PRODUCTS_PATH}/${product?.market}/${handleSubPathMarket(
-                product?.market,
-                product?.link,
-            )}`}
-        >
-            <a className="absolute-center flex-col">
-                <div className="absolute-center flex-col sm:py-3 sm:px-1 lg:p-5">
-                    <div className="smooth-effect relative z-10 font-primary hover:-translate-y-1 sm:h-[265px] sm:w-[155px] lg:h-[290px] lg:w-[185px]">
-                        <div
-                            className="absolute -z-10 rounded-2xl border-2 border-gray-700 sm:top-[6px] sm:left-[0px] sm:h-[265px] sm:w-[155px] lg:top-[6px] lg:left-[6px] lg:h-[290px] lg:w-[185px]"
-                            style={{
-                                backgroundColor:
-                                    mapping_market_colors[product?.market],
-                            }}
-                        ></div>
-                        <div className="relative z-20 flex cursor-pointer flex-col rounded-xl border-2 border-black bg-white hover:bg-white sm:h-[265px] sm:w-[155px] lg:h-[290px] lg:w-[185px]">
-                            <div className="relative space-y-4 p-2">
-                                {/* eslint-disable-next-line @next/next/no-img-element */}
-                                <img
-                                    src={product.img}
-                                    alt="img-item"
-                                    className="z-0 h-auto max-h-[150px] w-full rounded-xl"
-                                />
-                                <div className="absolute right-4 top-0 z-10 flex h-14 w-14 rounded-[50%] border border-black bg-white">
-                                    <button
-                                        className="absolute-center w-14 select-none"
-                                        onClick={activeNotification}
-                                    >
-                                        <BellIcon
-                                            className="w-12 stroke-1"
-                                            style={{
-                                                fill: isNotification
-                                                    ? '#fde047'
-                                                    : '#fff',
-                                            }}
-                                        />
-                                    </button>
-                                </div>
-                                <div className="flex flex-col px-2">
-                                    <h2 className="line-clamp-2">
-                                        {product.name}
-                                    </h2>
-                                    <div className="flex items-center space-x-2">
-                                        <h3 className="pt-1 text-2xl font-medium text-red-400 line-clamp-1 md:text-3xl">
-                                            <If
-                                                condition={isNumber(
-                                                    product.price,
-                                                )}
-                                            >
-                                                <Then>
-                                                    {new Intl.NumberFormat(
-                                                        'vi-VN',
-                                                        {
-                                                            style: 'currency',
-                                                            currency: 'VND',
-                                                        },
-                                                    ).format(
-                                                        Number(product.price),
-                                                    )}
-                                                </Then>
+        <div className="relative">
+            <If condition={isFavoriteProduct}>
+                <Then>
+                    <div className="absolute-center smooth-effect absolute right-0 top-0 z-40 flex h-14 w-14 rounded-[50%] border border-black bg-white hover:scale-110">
+                        <button
+                            onClick={handleRemoveProduct}
+                            className="absolute-center select-none p-4 "
+                        >
+                            <XMarkIcon className="h-8 w-8 stroke-1" />
+                        </button>
+                    </div>
+                </Then>
+            </If>
 
-                                                <Else>{product.price}</Else>
-                                            </If>
-                                        </h3>
-                                        <h3 className="m-auto pt-2 text-lg line-through">
-                                            <If
-                                                condition={isNumber(
-                                                    product?.priceBeforeDiscount,
-                                                )}
-                                            >
-                                                <Then>
-                                                    {new Intl.NumberFormat(
-                                                        'vi-VN',
-                                                        {
-                                                            style: 'currency',
-                                                            currency: 'VND',
-                                                        },
-                                                    ).format(
-                                                        Number(
-                                                            product?.priceBeforeDiscount,
-                                                        ),
-                                                    )}
-                                                </Then>
+            <Link
+                href={`/${PRODUCTS_PATH}/${
+                    product?.market
+                }/${handleSubPathMarket(product?.market, product?.link)}`}
+            >
+                <a className="absolute-center flex-col">
+                    <div className="absolute-center flex-col sm:py-3 sm:px-1 lg:p-5">
+                        <div className="smooth-effect relative z-10 font-primary hover:-translate-y-1 sm:h-[265px] sm:w-[155px] lg:h-[290px] lg:w-[185px]">
+                            <div
+                                className="absolute -z-10 rounded-2xl border-2 border-gray-700 sm:top-[6px] sm:left-[0px] sm:h-[265px] sm:w-[155px] lg:top-[6px] lg:left-[6px] lg:h-[290px] lg:w-[185px]"
+                                style={{
+                                    backgroundColor:
+                                        mapping_market_colors[product?.market],
+                                }}
+                            ></div>
+                            <div className="z-20 flex cursor-pointer flex-col rounded-xl border-2 border-black bg-white hover:bg-white sm:h-[265px] sm:w-[155px] lg:h-[290px] lg:w-[185px]">
+                                <div className="space-y-4 p-2">
+                                    {/* eslint-disable-next-line @next/next/no-img-element */}
+                                    <img
+                                        src={product.img}
+                                        alt="img-item"
+                                        className="z-0 h-auto max-h-[150px] w-full rounded-xl"
+                                    />
 
-                                                <Else>
-                                                    {
-                                                        product?.priceBeforeDiscount
-                                                    }
-                                                </Else>
-                                            </If>
-                                        </h3>
-                                    </div>
-                                    <div>
-                                        <h4
-                                            className="float-left py-2 text-xl"
-                                            style={{
-                                                color: mapping_market_colors[
-                                                    product?.market
-                                                ],
-                                            }}
-                                        >
-                                            {product?.market}
-                                        </h4>
-                                        <h5 className="float-right py-2 text-xl">
-                                            {product.totalSales}
-                                        </h5>
+                                    <div className="flex flex-col px-2">
+                                        <h2 className="line-clamp-2">
+                                            {product.name}
+                                        </h2>
+                                        <div className="flex items-center space-x-2">
+                                            <h3 className="pt-1 text-2xl font-medium text-red-400 line-clamp-1 md:text-3xl">
+                                                <If
+                                                    condition={isNumber(
+                                                        product.price,
+                                                    )}
+                                                >
+                                                    <Then>
+                                                        {new Intl.NumberFormat(
+                                                            'vi-VN',
+                                                            {
+                                                                style: 'currency',
+                                                                currency: 'VND',
+                                                            },
+                                                        ).format(
+                                                            Number(
+                                                                product.price,
+                                                            ),
+                                                        )}
+                                                    </Then>
+
+                                                    <Else>{product.price}</Else>
+                                                </If>
+                                            </h3>
+                                            <h3 className="m-auto pt-2 text-lg line-through">
+                                                <If
+                                                    condition={isNumber(
+                                                        product?.priceBeforeDiscount,
+                                                    )}
+                                                >
+                                                    <Then>
+                                                        {new Intl.NumberFormat(
+                                                            'vi-VN',
+                                                            {
+                                                                style: 'currency',
+                                                                currency: 'VND',
+                                                            },
+                                                        ).format(
+                                                            Number(
+                                                                product?.priceBeforeDiscount,
+                                                            ),
+                                                        )}
+                                                    </Then>
+
+                                                    <Else>
+                                                        {
+                                                            product?.priceBeforeDiscount
+                                                        }
+                                                    </Else>
+                                                </If>
+                                            </h3>
+                                        </div>
+                                        <div>
+                                            <h4
+                                                className="float-left py-2 text-xl"
+                                                style={{
+                                                    color: mapping_market_colors[
+                                                        product?.market
+                                                    ],
+                                                }}
+                                            >
+                                                {product?.market}
+                                            </h4>
+                                            <h5 className="float-right py-2 text-xl">
+                                                {product.totalSales}
+                                            </h5>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
 
-                <If condition={product?.qtyRemainPercent !== undefined}>
-                    <Then>
-                        <Progress
-                            done={100 - Number(product?.qtyRemainPercent)}
-                        />
-                    </Then>
-                </If>
-            </a>
-        </Link>
+                    <If condition={product?.qtyRemainPercent !== undefined}>
+                        <Then>
+                            <Progress
+                                done={100 - Number(product?.qtyRemainPercent)}
+                            />
+                        </Then>
+                    </If>
+                </a>
+            </Link>
+        </div>
     );
 };
 
--- src/pages/users/favorites.tsx
+++ src/pages/users/favorites.tsx
@@ -7,15 +7,23 @@ import Section from '~/components/shared/Section';
 import { MARKET_URL } from '~/constants';
 import useAxiosClient from '~/services/axiosClient';
 import ScrollTop from '~/components/shared/ScrollTop';
+import { ProductContextProvider } from '~/context/ProductContext';
+import { useCallback, useEffect, useState } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
 
 const FavoritesPage: NextPage = () => {
-    const { data: session } = useSession();
+    const { data: session, status } = useSession();
+    const [items, setItems] = useState<SearchResult[]>([]);
 
     const axiosClient = useAxiosClient(
         MARKET_URL('e-commerce-server' as Market),
     );
 
-    const { data: favoriteItems, isValidating } = useSWR<{
+    const {
+        data: favoriteItems,
+        isValidating,
+        mutate,
+    } = useSWR<{
         _id: string;
         favorite_products: SearchResult[];
     }>(
@@ -31,21 +39,87 @@ const FavoritesPage: NextPage = () => {
         },
     );
 
+    useEffect(() => {
+        if (favoriteItems) {
+            favoriteItems?.favorite_products &&
+                setItems(
+                    Array.from(favoriteItems?.favorite_products).reverse(),
+                );
+        }
+    }, [favoriteItems]);
+
+    const removeProduct = useCallback(
+        async (removeMode: 'single' | 'multiple', link?: string) => {
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            if (status === 'unauthenticated' || !session?.user?.id) return;
+
+            // Optimistic UI (remove ASAP in the UI)
+            toast.success('Xoá sản phẩm thành công');
+
+            if (removeMode === 'multiple') {
+                setItems([]);
+            } else {
+                setItems((prevItems) =>
+                    prevItems.filter((item) => item.link !== link),
+                );
+            }
+
+            try {
+                await axiosClient.delete(
+                    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                    // @ts-ignore
+                    `/users/${session?.user.id}/favorite`,
+                    {
+                        data: {
+                            link:
+                                removeMode === 'multiple' ? 'remove-all' : link,
+                        },
+                    },
+                );
+            } catch (error) {
+                toast.error('Không thành công! Xin hãy thử lại sau');
+
+                // restore item(s)
+                // handle steps: mutate (fetchAPI) -> all items will be set again
+                mutate();
+            }
+        },
+        [items],
+    );
+
     return (
         <ScrollTop>
-            <div className="min-h-screen w-full pt-[100px]">
-                <Section
-                    title="Sản phẩm đã yêu thích"
-                    style="my-10 w-max-[1300px] h-fit mx-auto w-[90%]"
-                >
-                    <ProductList
-                        isFetching={isValidating}
-                        items={favoriteItems?.favorite_products || []}
-                        isReachingEnd
-                        endMessage={' '}
-                    />
-                </Section>
-            </div>
+            <Toaster position="bottom-center" />
+
+            <ProductContextProvider value={{ removeProduct }}>
+                <div className="min-h-screen w-full pt-[100px]">
+                    <Section
+                        title="Sản phẩm đã yêu thích"
+                        style="my-10 w-max-[1300px] h-fit mx-auto w-[90%]"
+                    >
+                        <button
+                            onClick={() => removeProduct('multiple')}
+                            className="smooth-effect my-2 rounded-xl border-[1px] border-rose-500 px-4 py-2 hover:scale-110"
+                        >
+                            Xoá tất cả
+                        </button>
+
+                        <div className="full-size py-4">
+                            <ProductList
+                                isFavoriteProduct
+                                isFetching={isValidating}
+                                items={items}
+                                isReachingEnd
+                                endMessage={' '}
+                                emptyArrayMessage={
+                                    'Bạn chưa có sản phẩm nào yêu thích!'
+                                }
+                            />
+                        </div>
+                    </Section>
+                </div>
+            </ProductContextProvider>
         </ScrollTop>
     );
 };
