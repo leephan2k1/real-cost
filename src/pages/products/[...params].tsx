6057380b4290a5ff5fdc12138a460cbf199c199c
leephan2k1 <pttl2k1@gmail.com>
2022-11-14 19:17:26 +0700

    feat: 🎸 add related products section

--- src/pages/products/[...params].tsx
+++ src/pages/products/[...params].tsx
@@ -1,8 +1,9 @@
 import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
 import { Toaster } from 'react-hot-toast';
 import { Else, If, Then } from 'react-if';
-import { ItemHistory, Product } from 'types';
+import { ItemHistory, Product, ProductPreview } from 'types';
 import Voting from '~/components/features/Voting';
+import Head from '~/components/shared/Head';
 import ProductChart from '~/components/shared/ProductChart';
 import ProductDescription from '~/components/shared/ProductDescription';
 import ProductHead from '~/components/shared/ProductHead';
@@ -10,18 +11,22 @@ import ScrollTop from '~/components/shared/ScrollTop';
 import Section from '~/components/shared/Section';
 import {
     getHistoryPrice,
+    getRelatedProducts,
     getProductDetails as bcGetProductDetails,
 } from '~/serverless/models/Bc.model';
 import { getProductDetails as tkGetProductDetails } from '~/serverless/models/Tiki.model';
-import Head from '~/components/shared/Head';
+import Slides from '~/components/shared/Slides';
+import ProductCard from '~/components/shared/ProductCard';
 
 interface DetailsPageProps {
     product: Product;
+    relatedProducts?: ProductPreview[];
     productPriceHistory: ItemHistory | null;
 }
 
 const DetailsPage: NextPage<DetailsPageProps> = ({
     product,
+    relatedProducts,
     productPriceHistory,
 }) => {
     return (
@@ -43,6 +48,24 @@ const DetailsPage: NextPage<DetailsPageProps> = ({
                     <Section style="w-max-[1300px] h-fit mx-auto w-[90%]">
                         <ProductHead product={product} />
                     </Section>
+
+                    {relatedProducts && relatedProducts.length > 0 && (
+                        <Section
+                            title="Sản phẩm tương tự"
+                            style="my-10 w-max-[1300px] h-fit mx-auto w-[90%]"
+                        >
+                            <Slides>
+                                {relatedProducts.map((product) => {
+                                    return (
+                                        <ProductCard
+                                            key={product.link}
+                                            product={product}
+                                        />
+                                    );
+                                })}
+                            </Slides>
+                        </Section>
+                    )}
 
                     <Section
                         title="Người dùng Real Cost bình chọn"
@@ -110,10 +133,16 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
 
     const priceHistory = await getHistoryPrice(product?.product_base_id);
 
+    const relatedProducts = await getRelatedProducts(
+        String(product?.product_base_id),
+        url.join('/'),
+    );
+
     if (product) {
         return {
             props: {
                 product,
+                relatedProducts,
                 productPriceHistory: priceHistory,
             },
             revalidate: 60 * 60 * 12, //12h
